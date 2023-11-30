d0341afe68c297a109aa26312a9d502ce68a79a5
troycaselli <swordandtrowel1689@gmail.com>
2023-11-30 11:12:15 -0800

    added weighted assignments

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 
 // initialize variables - graded assignments 
-int totalAssignments = 5;
+int examAssignments = 5;
 
-int[] sophiaScores = new int[] {90, 86, 97, 98, 100};
-int[] andrewScores = new int[] {92, 89, 81, 96, 90};
-int[]  emmaScores = new int[] {70, 85, 87, 84, 68};
-int[] loganScores = new int[] {90, 95, 87, 88, 96};
-
-string[] studentNames = new string[] {"Sophia", "Andrew", "Emma", "Logan"};
+// student scores
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
 int[] studentScores = new int[10];
+
+// student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
 // print table header
 Console.WriteLine("Student\t\tScore\tGrade\n");
@@ -27,16 +33,32 @@ foreach (string name in studentNames)
         studentScores = andrewScores;
     else if (currentStudent == "Emma")
         studentScores = emmaScores;
-    else
+    else if (currentStudent == "Logan")
         studentScores = loganScores;
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
+    else
+        continue;
 
     // find average student score
     int scoresSum = 0;
+    int gradedAssignments = 0;
     foreach (int score in studentScores)
     {
-        scoresSum += score;
+        gradedAssignments++;
+        // determine weighting before adding
+        if (gradedAssignments <= examAssignments)
+            scoresSum += score;
+        else
+            scoresSum += score / 10;
     }
-    decimal studentScore = (decimal)scoresSum / totalAssignments;
+    decimal studentScore = (decimal)scoresSum / examAssignments;
 
     // determine student letter grade
     if (studentScore >= 97)
