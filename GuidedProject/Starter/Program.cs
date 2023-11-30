12dec95470ddbad27f32b3492e82b8fb37907e01
troycaselli <swordandtrowel1689@gmail.com>
2023-11-30 10:59:17 -0800

    updated scores and table headers

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -3,15 +3,16 @@
 // initialize variables - graded assignments 
 int totalAssignments = 5;
 
-int[] sophiaScores = new int[] {90, 86, 87, 98, 100};
+int[] sophiaScores = new int[] {90, 86, 97, 98, 100};
 int[] andrewScores = new int[] {92, 89, 81, 96, 90};
-int[]  emmaScores = new int[] {90, 85, 87, 98, 68};
+int[]  emmaScores = new int[] {70, 85, 87, 84, 68};
 int[] loganScores = new int[] {90, 95, 87, 88, 96};
 
 string[] studentNames = new string[] {"Sophia", "Andrew", "Emma", "Logan"};
 int[] studentScores = new int[10];
 
-Console.WriteLine("Student\t\tGrade\n");
+// print table header
+Console.WriteLine("Student\t\tScore\tGrade\n");
 
 // iterate through each student to print grades to console
 foreach (string name in studentNames)
